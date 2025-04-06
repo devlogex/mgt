6195131ffe9cf014fa3f4125081284f40765746a
tan.nguyen <tan.nguyen@parcelperform.com>
2025-04-06 18:39:10 +0700

    add start script

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+python manage.py migrate
+
+# Set container time
+unlink /etc/localtime
+ln -s /usr/share/zoneinfo/Universal /etc/localtime
+
+while true;
+do
+   echo "check pid app"
+   ps -ef | grep -v grep | grep -i -e application -e runserver
+   if [ $? -ne "0" ];then
+    echo "Service started"
+    python manage.py runserver 0.0.0.0:8000
+   else
+       echo " Application not started !!! Please wait on 5 sec to restart"
+   fi
+
+   sleep 5
+done
